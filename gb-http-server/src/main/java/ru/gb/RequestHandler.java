c62b8ecf8800f762a5725ef74d3ddac951ea656d
bafkit <konyrevpv@mail.ru>
2022-10-07 09:46:35 +0300

    Task of lesson 2

--- gb-http-server/src/main/java/ru/gb/RequestHandler.java
+++ gb-http-server/src/main/java/ru/gb/RequestHandler.java
@@ -1,36 +1,42 @@
 package ru.gb;
 
+import ru.gb.domain.HttpResponse;
+
 import java.io.IOException;
+import java.io.StringReader;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class RequestHandler implements Runnable {
 
     Config config;
-    InputHandler inputHandler;
-    OutputHandler outputHandler;
+    RequestParser requestParser;
+    ResponseSerializer responseSerializer;
     SocketService socketService;
 
     public RequestHandler(Config config, SocketService socketService) {
         this.config = config;
         this.socketService = socketService;
-        inputHandler = new InputHandler(socketService);
-        outputHandler = new OutputHandler(socketService);
     }
 
     @Override
     public void run() {
 
-        String[] parts = new String[0];
-        try {
-            parts = inputHandler.getInput();
-        } catch (IOException e) {
-            e.printStackTrace();
+        Path path = Paths.get(config.getWWW(), requestParser.parse(socketService.readRequest()).getPath());
+
+        if (!Files.exists(path)) {
+            HttpResponse httpResponse = new HttpResponse(404, "<h1>Файл не найден!</h1>\n");
+            socketService.writeResponse(responseSerializer.serialize(httpResponse), new StringReader(httpResponse.getBody()));
+            return;
         }
 
-        Path path = Paths.get(config.getWWW(), parts[1]);
-
-        outputHandler.output(path);
+        try {
+            HttpResponse httpResponse = new HttpResponse(200, Files.newBufferedReader(path).toString());
+            socketService.writeResponse(responseSerializer.serialize(httpResponse), new StringReader(httpResponse.getBody()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
         try {
             socketService.close();
