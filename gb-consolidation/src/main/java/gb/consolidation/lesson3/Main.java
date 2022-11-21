956113ff20eafdd74f8accf346c01cb1e6a88ffa
bafkit <konyrevpv@mail.ru>
2022-11-21 18:28:29 +0300

    Task 3

---
+++ gb-consolidation/src/main/java/gb/consolidation/lesson3/Main.java
@@ -0,0 +1,9 @@
+package gb.consolidation.lesson3;
+
+public class Main {
+
+    public static void main(String[] args) {
+        PingPongApp pingPongApp = new PingPongApp();
+        pingPongApp.play();
+    }
+}
